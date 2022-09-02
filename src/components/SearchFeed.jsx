420a807522ee93e4d755a6dbddd2e5acf145d97e
Atharva Mali <atharva.mali3081@gmail.com>
2022-09-02 12:58:39 +0530

    Implemented search bar

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -17,7 +17,7 @@ const SearchFeed = () => {
   return (
     <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
       <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
-        Search Results for:{" "}
+        Search Results for:{""}
         <span style={{ color: "#FC1503" }}>{searchTerm}</span> videos
       </Typography>
       <Videos videos={videos} />
